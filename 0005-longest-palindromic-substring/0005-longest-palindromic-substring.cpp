b1dfa5c06dfdcac62229318e87070a22981ee048
kenloumixx <109455679+kenloumixx@users.noreply.github.com>
2025-03-28 13:19:17 +0900

    Time: 16 ms (62.45%), Space: 18.6 MB (53.36%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,29 +2,34 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int len = s.length();
-        if(len<=1) return s;
-
-        auto expand_from_center = [&](int left, int right) {
-            while (left >= 0 && right < len && s[left] == s[right]) {
+        if (len == 1)
+        {
+            return s;
+        }
+        auto expand_from_center = [&](int left, int right){
+            while (left >= 0 && right < len && s[left] == s[right])
+            {
                 left--;
                 right++;
             }
-            return s.substr(left + 1, right - left - 1);
+            return s.substr(left+1, right - left - 1);
         };
 
-        string max_str = s.substr(0, 1);
-        for (int i = 0; i < len - 1; i++) {
-            string odd = expand_from_center(i, i);
-            string even = expand_from_center(i, i + 1);
+        std::string max_str = "";
+        for (int i = 0; i < len - 1; i++) 
+        {
+            std::string odd = expand_from_center(i, i);
+            std::string even = expand_from_center(i, i+1);
 
-            if (odd.length() > max_str.length()) {
+            if (odd.length() > max_str.length())
+            {
                 max_str = odd;
             }
-            if (even.length() > max_str.length()) {
+            if (even.length() > max_str.length())
+            {
                 max_str = even;
             }
         }
-
         return max_str;
     }
 };
