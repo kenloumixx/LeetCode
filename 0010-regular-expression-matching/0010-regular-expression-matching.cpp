8bac6dd745f5c309c0f1dbe6ced72942ed4ffd54
kenloumixx <109455679+kenloumixx@users.noreply.github.com>
2025-03-30 12:06:06 +0900

    Time: 3 ms (48.07%), Space: 9.2 MB (36.93%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    bool rec(std::string ss, std::string pp) {
-        if (ss.empty() && pp.empty()) return true;
-        else if (pp.empty()) return false;
-        int ppLen = pp.length();
-        int ssLen = ss.length();
-        if (ppLen > 1 && pp[1] == '*')
+    bool solveMem(int i, int j, const std::string& s, const std::string& p, std::vector<std::vector<std::optional<bool>>>& dp) {
+        if (j == p.length()) return i == s.length();
+        if (dp[i][j].has_value()) return dp[i][j].value();
+
+        bool firstCharMatch = (i < s.length()) && (s[i] == p[j] || p[j] == '.');
+
+        if (j + 1 < p.length() && p[j + 1] == '*')
         {
-            if (rec(ss, pp.substr(2)) == true) return true;
-            if (ssLen > 0 && (ss[0] == pp[0] || pp[0] == '.')) return rec(ss.substr(1), pp);
-        } else 
-        {
-            if (ssLen > 0 && (ss[0] == pp[0] || pp[0] == '.')) return rec(ss.substr(1), pp.substr(1));
+            bool take = firstCharMatch && solveMem(i+1, j, s, p, dp);
+            bool not_take = solveMem(i, j+2, s, p, dp);
+            dp[i][j] = take || not_take;
+            return dp[i][j].value();
         }
-        return false;
+
+        dp[i][j] = firstCharMatch && solveMem(i+1, j+1, s, p, dp);
+        return dp[i][j].value();
     }
 
     bool isMatch(string s, string p) {
-        return rec(s, p);
+        std::vector<std::vector<std::optional<bool>>> dp(s.length() + 1, std::vector<std::optional<bool>> (p.length() + 1));
+        return solveMem(0, 0, s, p, dp);
     }
 };
